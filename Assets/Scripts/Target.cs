6542cce93e0106b6217d6d42334f6b0ddc5932e7
santifont <sanalffer@alu.edu.gva.es>
2025-11-13 09:19:56 +0100

    Update Target.cs

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,11 +5,9 @@ public class Target : MonoBehaviour
 {
     private int state = 0;
     private Renderer renderer;
-    private float rotationSpeed = 2.5f;
+    private float rotationSpeed = 5f;
     private Vector3 rotationAngle = new Vector3(0, 45.0f, 0);
     private GameObject randomPosition; // Random Position
-    public GameObject targetPrefab;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,8 +16,6 @@ public class Target : MonoBehaviour
         // Random Position
         randomPosition = GameObject.Find("Pos" + Random.Range(1, 6));
         Debug.Log(randomPosition.name);
-
-        transform.position = randomPosition.transform.position;
     }
 
     // Update is called once per frame
@@ -34,7 +30,6 @@ public class Target : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         state++;
-        transform.position = randomPosition.transform.position;
 
         if (state == 1)
         {
