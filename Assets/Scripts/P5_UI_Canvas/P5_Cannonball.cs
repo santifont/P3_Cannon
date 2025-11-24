c809ef74f48cce26f8f8df0d59bcd2846f42b514
santifont <sanalffer@alu.edu.gva.es>
2025-11-24 11:19:18 +0100

    Minor changes

---
+++ Assets/Scripts/P5_UI_Canvas/P5_Cannonball.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class P5_Cannonball : MonoBehaviour
+{
+    private Rigidbody rb;
+    private GameObject startPoint;
+    private GameObject finishPoint;
+    private int shootingForce = 200;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        startPoint = GameObject.Find("StartPoint");
+        finishPoint = GameObject.Find("FinishPoint");
+
+        Vector3 direccion = finishPoint.transform.position - startPoint.transform.position;
+        //rb.AddForce(direccion * shootingForce * GameObject.Find("Green_Button").GetComponent<P5_Cannon>().potenciaAcumulada);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/P5_UI_Canvas/P5_GreenButton.cs
+++ Assets/Scripts/P5_UI_Canvas/P5_GreenButton.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 using TMPro;
 public class P5_GreenButton : MonoBehaviour
 {
-    private GameObject cannon;
-    private GameObject potenciaCanvas;
+    private float           tiempoInicio;
+    private float            tiempoFinal;
+    public  float      potenciaAcumulada;
+    private Color           initialColor;
+    private Renderer            renderer;
+    private GameObject            cannon;
+    private GameObject    potenciaCanvas;
     private TextMeshProUGUI potenciaText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        initialColor = GetComponent<Renderer>().material.color;
+        renderer = GetComponent<Renderer>();
         cannon = GameObject.Find("CannonMachine");
         potenciaCanvas = GameObject.Find("Potencia Canvas");
         potenciaText = potenciaCanvas.GetComponent<TextMeshProUGUI>();
@@ -22,11 +29,19 @@ public class P5_GreenButton : MonoBehaviour
 
     void OnMouseDown()
     {
-        //cannon.GetComponent<P5_Cannon>().Shoot();
+        renderer.material.color = Color.red;
+        tiempoInicio = Time.time;
+        potenciaText.text = "Potencia: " + tiempoInicio;
+
     }
 
     private void OnMouseUp()
     {
-        
+        renderer.material.color = initialColor;
+        tiempoFinal = Time.time;
+        potenciaText.text = "Potencia: " + tiempoFinal;
+
+        potenciaAcumulada = tiempoFinal - tiempoInicio;
+        cannon.GetComponent<P5_Cannon>().Shoot();
     }
 }
